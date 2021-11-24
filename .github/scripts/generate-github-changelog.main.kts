068b3769014b5b183668b478d1d0b13f3f8dc823
Mahdi Hosseinzadeh <mdihosseinzadeh@gmail.com>
2021-11-24 19:54:06 +0330

    !!!!!!!!!!!Temp10!!!!!!!!!!!

--- .github/scripts/generate-github-changelog.main.kts
+++ .github/scripts/generate-github-changelog.main.kts
@@ -1,25 +1,12 @@
 #!/usr/bin/env kotlin
 
-@file:JvmName("Scraper")
-@file:CompilerOptions("-jvm-target", "11")
-@file:Repository("https://repo.maven.apache.org/maven2")
-@file:Repository("https://jcenter.bintray.com")
-@file:Repository("https://jitpack.io")
-@file:DependsOn("com.beust:klaxon:5.5")
-
 import java.io.File
 
-
-// TODO: Remove me
-args.forEach(::println)
-
-val modifiedRelFile = args
-    .map(::File)
-    .single { "RelNotes" in it.parent }
-
-// TODO: Remove me
-println("modified = $modifiedRelFile")
-
 val outputFile = File("release-notes.txt")
+val releaseFile = args
+    .onEach(::println)
+    .map(::File)
+    .singleOrNull { "RelNotes" in it.parent }
 
-modifiedRelFile.copyTo(outputFile)
+releaseFile?.copyTo(outputFile)
+    ?: println("No release note file change in this commit")
